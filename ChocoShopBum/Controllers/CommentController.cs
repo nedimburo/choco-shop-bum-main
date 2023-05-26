e8a4a496b2a1a8a7aceb8065709815ffc4b840fc
Nedim Buro <nedimb9402@gmail.com>
2023-05-26 22:40:43 +0200

    Dodana nova funkcionalnost za unos i prikaz komentara.

---
+++ ChocoShopBum/Controllers/CommentController.cs
@@ -0,0 +1,42 @@
+﻿using ChocoShopBum.Areas.Identity.Data;
+using ChocoShopBum.Data;
+using ChocoShopBum.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChocoShopBum.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CommentController(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Comment> commentList = _db.Comments;
+            return View(commentList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // za post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Comment obj)
+        {
+            obj.UserID = _userManager.GetUserId(this.User);
+            _db.Comments.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ChocoShopBum/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using ChocoShopBum.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChocoShopBum.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentID { get; set; }
+        public string UserID { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+        [ForeignKey("UserID")]
+        public virtual ApplicationUser User { get; set; }
+    }
+}
